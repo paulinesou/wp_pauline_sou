0309469550ad16993f103645537ac093bde57447
PaulineSou <pauline.soudry@gmail.com>
2023-06-23 16:49:04 +0200

    premier commit

---
+++ astra_child/assets/js/script.js
@@ -0,0 +1,39 @@
+// EVENEMENT AU CLIC : BOUTON DETAIL PERSONNAGE
+
+const buttonPerso = document.querySelector('#select-perso');
+const avatar = document.querySelector('#avatar');
+const identite = document.querySelector('#identite-perso');
+const equipement = document.querySelector('#equipements');
+const competence = document.querySelector('#competences');
+const legende = document.querySelector('#legend-avatar');
+
+buttonPerso.addEventListener('click', () => {
+	avatar.classList.add('active')
+	identite.classList.add('active')
+	equipement.classList.add('active')
+	competence.classList.add('active')
+	legende.classList.add('active')
+});
+
+
+// ANIMATION : APPARITION SECTION HISTOIRE AU SCROLL
+
+const targets = document.querySelectorAll('#histoire');
+
+function handleIntersection(entries) {
+    entries.map((entry) => {
+      entry.target.classList.remove('active')
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active')
+        } else {
+            entry.target.classList.remove('active')
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersection);
+targets.forEach(target => observer.observe(target));
+
+
+
+
---
+++ astra_child/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles(){
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    // Chargement du themeChild.css thème personnalisé
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
+    // Chargement de mon script.js thème enfant
+    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/assets/js/script.js', array('jquery'), '1.0', true);
+    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/particles.js', array('jquery'), '1.0', true);
+    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/app.js', array('jquery'), '1.0', true);
+}
+?>
